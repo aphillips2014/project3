862648fde96a846ae0bab894299e1ada925ca83e
Andrew Phillips <phillips4energy@gmail.com>
2015-05-25 09:04:05 -0500

    First set  of fixes for Code Review #1
    Added semicolons where needed
    Made indentation uniform for class methods, functions
    Aligned if and for statements. Added a TODO
    Removed underscores from checkCollision parameters
    Removed underscores from getRand paramter

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-//console.log("in app.js ...");
-
 /*************************************************************
  Class: Enemy
  Parameters: sprite, x location, y location, horizontal speed
@@ -8,94 +6,82 @@
 **************************************************************/
 
 var Enemy = function(sprite,x,y,speed) {
-    //this = Object.create(Enemy.prototype); //returns "this" which we can add and set properties for our class
-
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.x = x;
-    this.y = y;
-    this.sprite = sprite;
-    this.speed = speed;
-    this.offscreenLeftPosStart = -100;
-}
+      this.x = x;
+      this.y = y;
+      this.sprite = sprite;
+      this.speed = speed;
+      this.offscreenLeftPosStart = -100;
+};
 
 /***********************************************************************
-    Method: update
-    Description: Update the enemy's position, required method for game
-    Parameter: dt, a time delta between ticks
-    Called by: updateEntities function in engine.js script
+  Method: update
+  Description: Update the enemy's position, required method for game
+  Parameter: dt, a time delta between ticks
+  Called by: updateEntities function in engine.js script
 ************************************************************************/
 
 Enemy.prototype.update = function(dt) {
-    // console.log("Enemy dt is:" + dt);
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for all computers.
 
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+      this.x = this.x + (dt * this.speed);
 
-       this.x = this.x + (dt * this.speed);
+      //When enemy position is greater than the width of play area set
+      //left side start of image and mkeake sure the Enemy transitions
+      //smoothly from left and not just popping onto screen
 
-       //When enemy position is greater than the width of play area set left side start of image
-       if(this.x > ctx.canvas.clientWidth){
-           //Make sure the Enemy transitions smoothly from left and not just popping onto screen
-           this.x = this.offscreenLeftPosStart; 
-       }
-}
+      if (this.x > ctx.canvas.clientWidth){
+          this.x = this.offscreenLeftPosStart;
+      }
+};
 
 /***********************************************************************
-    Method: render
-    Description: Draw the Enemy on the screen. Required method for game
-    Parameter: none
-    Called by: renderEntities function in engine.js script
+  Method: render
+  Description: Draw the Enemy on the screen. Required method for game
+  Parameter: none
+  Called by: renderEntities function in engine.js script
 ************************************************************************/
 
 Enemy.prototype.render = function() {
-    //console.log("******************** in Enemy.render *****************");
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
-}
-
-/***********************************************************************
-    Method: getRand
-    Description: Create a random number
-    Parameter: seed value
-    Called by: enemy object 
-************************************************************************/
-
-Enemy.prototype.getRand = function(_rnd){
-   var randomnumber=((Math.random() + 10) * _rnd);
-   var randomnumber2=Math.ceil(randomnumber);
-   return randomnumber2;
+      //console.log("******************** in Enemy.render *****************");
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+/***********************************************************************
+  Method: getRand
+  Description: Create a random number
+  Parameter: seed value
+  Called by: enemy object
+************************************************************************/
+
+Enemy.prototype.getRand = function(rnd){
+      var randomnumber = ((Math.random() + 10) * rnd);
+      var randomnumber2 = Math.ceil(randomnumber);
+      return randomnumber2;
+};
 
 /*************************************************************
  Class: Player
  Parameters: none
  Description: Player class constructor that initializes our
-              player .This class requires an update(), render() 
+              player .This class requires an update(), render()
               and a handleInput() method.
  Called by: app.js
 **************************************************************/
 
 var Player = function() {
-    //console.log("In Player constructor");
-    //this = Object.create(Player.prototype);
+      //console.log("In Player constructor");
+      //this = Object.create(Player.prototype);
 
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+      this.sprite = 'images/char-boy2.png';
+      this.x = 430/2 - 10; //position player sprite horizontally
+      this.y = 606 - Math.ceil((85 + (85/2))); //position player sprite vertically
+      this.boundBoxVar = 52;
 
-    this.sprite = 'images/char-boy2.png';
-    this.x = 430/2 - 10; //position player sprite horizontally
-    this.y = 606 - Math.ceil((85 + (85/2))); //position player sprite vertically
-    this.boundBoxVar = 52;
+      //  TODO: this.gameOver = 'images/game-over.png';
+      //  TODO: this.playerHeight = Resources.get(this.sprite).height;
 
-    //this.gameOver = 'images/game-over.png';
-    //this.playerHeight = Resources.get(this.sprite).height;
-  
-}
+};
 
 /***********************************************************************
     Method: update
@@ -105,39 +91,40 @@ var Player = function() {
 ************************************************************************/
 
 Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    
-    //X Position
-    var playerXpositionEndPointRight = Resources.canvas.width - Resources.get(this.sprite).width;
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
 
-    //Y Position
-    var height = Resources.get(this.sprite).height;
-    var playerYpositionEndPointBottom = Resources.canvas.height - Math.ceil((height + (height/2)));
-    //var boundBoxVar = 52;
+      //X Position
+      var playerXpositionEndPointRight = Resources.canvas.width - Resources.get(this.sprite).width;
 
-    //To keep the player from moving of the right side of screen subtract the width of image from the width of canvas 
-    //which will set the end point on right side
-    //When player reaches that x position set the current player position to same value to give appearance
-    //that player can't go off edge
-    if(this.x > playerXpositionEndPointRight){
-           this.x = playerXpositionEndPointRight; 
-    }
-    if(this.x < 0.5){
-           this.x = 0.5; 
-    }
+      //Y Position
+      var height = Resources.get(this.sprite).height;
+      var playerYpositionEndPointBottom = Resources.canvas.height - Math.ceil((height + (height/2)));
 
-    if(this.y > playerYpositionEndPointBottom){
-           this.y = playerYpositionEndPointBottom; 
-    }
+      //To keep the player from moving of the right side of screen subtract the width of image from the width of canvas
+      //which will set the end point on right side
+      //When player reaches that x position set the current player position to same value to give appearance
+      //that player can't go off edge
+      if (this.x > playerXpositionEndPointRight){
+          this.x = playerXpositionEndPointRight;
+      }
 
-    //52px places the position of the player just above the bounding box of the enemy 
-    //once the player reaches the water this prevents contact due to space limits of bounding box and position of enemy
-    if(this.y < this.boundBoxVar){
-           this.y = 51; 
-    }
-}
+      if (this.x < 0.5){
+          this.x = 0.5;
+      }
+
+      if (this.y > playerYpositionEndPointBottom){
+          this.y = playerYpositionEndPointBottom;
+      }
+
+      //52px places the position of the player just above the bounding box of the enemy
+      //once the player reaches the water this prevents contact due to space limits of
+      //bounding box and position of enemy
+      if (this.y < this.boundBoxVar){
+          this.y = 51;
+      }
+};
 
 /***********************************************************************
     Method: render
@@ -147,10 +134,10 @@ Player.prototype.update = function(dt) {
 ************************************************************************/
 
 Player.prototype.render = function() {
-    //console.log("X location of player is: " + this.x);
-    //console.log("Y location of player is: " + this.y);
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+      //console.log("X location of player is: " + this.x);
+      //console.log("Y location of player is: " + this.y);
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 /***********************************************************************
     Method: handleInput
@@ -161,39 +148,39 @@ Player.prototype.render = function() {
 
 Player.prototype.handleInput = function(keyCode) {
 
-    var spriteHeight = Resources.get(this.sprite).height;
-    var spriteWidth = Resources.get(this.sprite).width;
-    var step = 2; //the amount of movement up or down
+      var spriteHeight = Resources.get(this.sprite).height;
+      var spriteWidth = Resources.get(this.sprite).width;
+      var step = 2; //the amount of movement up or down
 
-    //Divide the spriteHeight by step to give player smaller movements vertically 
-    if(keyCode === 'up'){
-        this.y -= spriteHeight/step;
-    }
-    if(keyCode === 'down'){
-        this.y += spriteHeight/step;
-    }
-    if(keyCode === 'left'){
-        this.x -= spriteWidth;
-    }
-    if(keyCode === 'right'){
-        this.x += spriteWidth;
-    }
-     
-    //If player hits the water set to true to put player in start position 
-    //in the water if player collides with a enemy on the way back to grass 
-    if(this.y <= this.boundBoxVar){
-        Player.WATER = true;
-    }
-    
-    //If player hasn't hit the water set to false to put player in start position 
-    //in the in grass until the player hits the water
-    if(this.y >= 480){
-        Player.WATER = false;
-    }
+      //Divide the spriteHeight by step to give player smaller movements vertically
+      if (keyCode === 'up'){
+          this.y -= spriteHeight/step;
+      }
 
+      if (keyCode === 'down'){
+          this.y += spriteHeight/step;
+      }
 
+      if (keyCode === 'left'){
+          this.x -= spriteWidth;
+      }
 
-}
+      if (keyCode === 'right'){
+          this.x += spriteWidth;
+      }
+
+      //If player hits the water set to true to put player in start position
+      //in the water if player collides with a enemy on the way back to grass
+      if (this.y <= this.boundBoxVar){
+          Player.WATER = true;
+      }
+
+      //If player hasn't hit the water set to false to put player in start position
+      //in the in grass until the player hits the water
+      if (this.y >= 480){
+          Player.WATER = false;
+      }
+};
 
 
 /***********************************************************************
@@ -203,76 +190,72 @@ Player.prototype.handleInput = function(keyCode) {
     Called by: update function in engine.js script
 ************************************************************************/
 
-function checkCollisions(_enemy, _player, _res){
-         //console.log("in checkCollisions");
-         //Enemy = a
-         //Player  = b
-         
-         var enemy;
-         var player = _player;
-         var enemyHeight;
-         var enemyWidth;
-         var playerHeight;
-         var playerWidth;
-         var enemyLength = _enemy.length;
+function checkCollisions(enemyCol, playerCol, res){
+      //console.log("in checkCollisions");
+      //Enemy = a
+      //Player  = b
 
-         var coll1;
-         var coll2;
-         var coll3;
-         var coll4;
+      //Enemy collision variables
+      var enemy;
+      var enemyHeight;
+      var enemyWidth;
+      var enemyLength = enemyCol.length;
 
-         var collisionResult;
-         
-         //Loop through enemy array and calculate 
-         //collision for each enemy against the player
-         for(i = 0; i < enemyLength; i++){
-            enemy = _enemy[i];
-            enemyHeight = _res.get(enemy.sprite).height;
-            enemyWidth = _res.get(enemy.sprite).width;
-            playerHeight = _res.get(player.sprite).height;
-            playerWidth = _res.get(player.sprite).width;
+      //Player collision variables
+      var player = playerCol;
+      var playerHeight;
+      var playerWidth;
 
-            //Collision algorithm provide by
-            //http://stackoverflow.com/questions/2440377/javascript-collision-detection
-            coll1 = (enemy.y + enemyHeight) < player.y;
-            coll2 = enemy.y > (player.y + playerHeight);
-            coll3 = (enemy.x + enemyWidth) < player.x;
-            coll4 = enemy.x > (player.x + playerWidth);
+      //Collision variables
+      var coll1;
+      var coll2;
+      var coll3;
+      var coll4;
+      var collisionResult;
 
-            //Short circuit on true value to signal no collision
-            //If no short circuit than there is a collison on one of the enemies
-            //so turn the result to true
-            collisionResult = !(coll1 || coll2 || coll3 || coll4);
+      //Loop through enemy array and calculate
+      //collision for each enemy against the player
+      for (var i = 0; i < enemyLength; i++){
+          enemy = enemyCol[i];
+          enemyHeight = res.get(enemy.sprite).height;
+          enemyWidth = res.get(enemy.sprite).width;
+          playerHeight = res.get(player.sprite).height;
+          playerWidth = res.get(player.sprite).width;
 
-            if(collisionResult){
-               reset();
-            }
+          //Collision algorithm provide by
+          //http://stackoverflow.com/questions/2440377/javascript-collision-detection
+          coll1 = (enemy.y + enemyHeight) < player.y;
+          coll2 = enemy.y > (player.y + playerHeight);
+          coll3 = (enemy.x + enemyWidth) < player.x;
+          coll4 = enemy.x > (player.x + playerWidth);
 
-         }
-         
-        //Debugger info
-         if(0){
-            sleep(1200);
-            console.log("coll1 is : " + coll1);
-            console.log("coll2 is : " + coll2);
-            console.log("coll3 is : " + coll3);
-            console.log("coll4 is : " + coll4);
+          //Short circuit on true value to signal no collision
+          //If no short circuit than there is a collison on one of the enemies
+          //so turn the result to true
+          collisionResult = !(coll1 || coll2 || coll3 || coll4);
 
-            console.log("Enemy x location is: " + enemy.x);
-            console.log("Enemy y location is: " + enemy.y);
+          if (collisionResult){
+              reset();
+          }
+      }
 
-            console.log("Enemy height is: " + enemyHeight);
-            console.log("Enemy width is: " + enemyWidth);
-            console.log("Player height is: " + playerHeight);
-            console.log("Player width is: " + playerWidth);
+      //Debugger info
+      if (false){
+          sleep(1200);
+          console.log("coll1 is : " + coll1);
+          console.log("coll2 is : " + coll2);
+          console.log("coll3 is : " + coll3);
+          console.log("coll4 is : " + coll4);
 
-            console.log("Player x location is: " + player.x);
-            console.log("Player y location is: " + player.y);
-         }   
-
-        //Enemy = a
-        //Player  = b
-
+          console.log("Enemy x location is: " + enemy.x);
+          console.log("Enemy y location is: " + enemy.y);
+          console.log("Enemy height is: " + enemyHeight);
+          console.log("Enemy width is: " + enemyWidth);
+          console.log("Player height is: " + playerHeight);
+          console.log("Player width is: " + playerWidth);
+          console.log("Player x location is: " + player.x);
+          console.log("Player y location is: " + player.y);
+      }
 }
 
 /***********************************************************************
@@ -285,24 +268,25 @@ function checkCollisions(_enemy, _player, _res){
 ************************************************************************/
 
 function reset(){
-    //console.log("Player.WATER in reset is: " + Player.WATER);
-    if(Player.WATER === true){
-        player.y = 52; //position player in water
-    }else{
-        player.y = 480;//position player in grass
-    }
-    player.x = 430/2 - 10;
+      //console.log("Player.WATER in reset is: " + Player.WATER);
+      if (Player.WATER === true){
+          player.y = 52; //position player in water
+      } else {
+          player.y = 480;//position player in grass
+      }
+      player.x = 430/2 - 10;
 }
 
 
 //Sleep script http://www.phpied.com/sleep-in-javascript/
 function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
+      var start = new Date().getTime();
+
+      for (var i = 0; i < 1e7; i++) {
+          if ((new Date().getTime() - start) > milliseconds){
+              break;
+          }
+      }
 }
 
 
@@ -321,17 +305,17 @@ var allEnemies = [enemy1,enemy2,enemy3,enemy4];
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+      var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
-    };
+      };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+      player.handleInput(allowedKeys[e.keyCode]);
 });
 
-/*
+/* TODO
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
 */
 /*clickLocations = [];
